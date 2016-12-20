237ebde44d8b91711b5c379ab7d6281d6409cd01
ZareMendozaC <zare.mendozac@gmail.com>
2016-12-20 10:24:32 -0500

    finish exercise 3 and adding styles

---
+++ main.js
@@ -0,0 +1,5 @@
+function sueldoPorHoras() {
+	var num1 = parseInt(document.getElementById('n1').value);
+	var num2 = parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value = num1 * num2;
+}
